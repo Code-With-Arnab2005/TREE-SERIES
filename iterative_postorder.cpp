ca9f0fcca1cb8fea0480261d5c1c77fccfb1f229
Arnab Bhattacharya <arnabbhattacharya2005@gmail.com>
2025-08-20 15:53:33 +0530

    add postorder iterative traversal

---
+++ iterative_postorder.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<algorithm>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        val = value;
+        left = right = nullptr;
+    }
+};
+
+vector<int> postorder_iterative(Node* root){
+    vector<int> ans;
+    stack<Node*> st;
+    if(root) st.push(root);
+    while(!st.empty()){
+        Node* node = st.top();
+        st.pop();
+        if(node->left) st.push(node->left);
+        if(node->right) st.push(node->right);
+        ans.push_back(node->val);
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+
+int main(){
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+
+    root->right = new Node(3);
+    root->right->left = new Node(5);
+    root->right->right = new Node(6);
+
+    root->right->left->left = new Node(7);
+
+    cout<<"Post Order Iterative Traversal: "<<endl;
+    vector<int> ans = postorder_iterative(root);
+    for(int &it: ans){
+        cout<<it<<" ";
+    }
+}
